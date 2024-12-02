75781361488a9847266acceefa64461879386115
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-02 09:45:27 +0100

    feat:ciclo

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*
+Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
+
+//stampare con console.log numeri da 1 a 100
+//per multipli di 3 segnare come Fizz
+//per i multipli di 5 segnare Buzz
+//per i multipli di entrambi segnare FizzBuzz
+//altrimenti stampare il numero
+
+for(let i = 1;i <= 10; i++){
+
+    if((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("FizzBuzz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
+}
