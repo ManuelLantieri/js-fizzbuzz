793541940ae08a3ea1c2832751f7e096affff82e
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-02 10:15:15 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -17,14 +17,14 @@ Consigli del giorno:
 //per i multipli di entrambi segnare FizzBuzz
 //altrimenti stampare il numero
 
-for(let i = 1;i <= 10; i++){
+for(let i = 1; i <= 100; i++){
 
-    if((i % 3 === 0) && (i % 5 === 0)) {
+     if (i % 3 ===0 && i % 5 === 0){
         console.log("FizzBuzz")
+     } else if(i % 3 === 0) {
+        console.log("Fizz");
     } else if (i % 5 === 0) {
-        console.log("Buzz")
-    } else if (i % 3 === 0) {
-        console.log("Fizz")
+        console.log("Buzz");
     } else {
         console.log(i)
     }
